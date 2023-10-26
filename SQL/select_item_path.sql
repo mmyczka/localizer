1e556354b23d7a1c10851324ef9e54436b4d25fa
MMy <m.myczka@gmail.com>
2023-10-26 21:34:24 +0200

    Select path for an item

---
+++ SQL/select_item_path.sql
@@ -0,0 +1,29 @@
+WITH ItemPath AS (
+    SELECT
+        id,
+        name,
+		parent_id,
+        CAST(name AS VARCHAR(MAX)) AS path,
+		ROW_NUMBER() OVER (ORDER BY id) as row
+    FROM
+        items
+    WHERE
+        name = 'Hammer'  -- Replace 'Hammer' with the item you're searching for
+    UNION ALL
+    SELECT
+        i.id,
+        i.name,
+		i.parent_id,
+        CONCAT(i.name, ' -> ', p.path),
+		p.row + 1 as row
+    FROM
+        items i
+    JOIN
+        ItemPath p ON i.id = p.parent_id
+	Where p.parent_id is not null
+)
+SELECT TOP 1
+    path
+FROM
+    ItemPath
+ORDER BY row DESC;
